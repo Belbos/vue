b4b366452cac0ea99d5752f7ebaf9be987aefc85
crachi <crachi@naver.com>
2018-11-13 23:59:12 +0900

    dt-project update
    dt-project update

---
+++ 42.dt-project/vue-springBoot/frontend/src/components/Main.vue
@@ -0,0 +1,263 @@
+<template>
+  <section class="content">
+    <row>
+      <div class="col-md-4">
+        <div class="box">
+          <!-- <div class="box-header with-border">
+            <h3 class="box-title">Latest Orders</h3>
+          </div> -->
+          <div class="box-body">  
+           <va-calendar></va-calendar>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">Latest Orders</h3>
+          </div>
+          <div class="box-body">  
+            <p class='text-center'>
+              <strong>Test Bar Chart</strong>
+            </p>
+            <va-chart
+              :chart-config='barChartConfig'
+            >
+            </va-chart>
+          </div> 
+        </div>   
+      </div>
+      <div class="col-md-4">
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">Latest Orders</h3>
+          </div>
+          <div class="box-body">  
+            <p class='text-center'>
+              <strong>Test Doughnut Chart</strong>
+            </p>
+            <va-chart
+              :chart-config='doughnutChartConfig'
+            >
+            </va-chart>
+          </div>  
+        </div>
+      </div>
+    </row>
+    <row>
+      <div class="col-md-8">
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">Latest Orders</h3>
+          </div>
+         <div class="box-body">
+            <div class="table-responsive">
+              <table class="table no-margin">
+                <thead>
+                <tr>
+                  <th>Order ID</th>
+                  <th>Item</th>
+                  <th>Status</th>
+                  <th>Popularity</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td><a href="pages/examples/invoice.html">OR9842</a></td>
+                  <td>Call of Duty IV</td>
+                  <td><span class="label label-success">Shipped</span></td>
+                  <td>
+                    <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="pages/examples/invoice.html">OR1848</a></td>
+                  <td>Samsung Smart TV</td>
+                  <td><span class="label label-warning">Pending</span></td>
+                  <td>
+                    <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                  <td>iPhone 6 Plus</td>
+                  <td><span class="label label-danger">Delivered</span></td>
+                  <td>
+                    <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                  <td>Samsung Smart TV</td>
+                  <td><span class="label label-info">Processing</span></td>
+                  <td>
+                    <div class="sparkbar" data-color="#00c0ef" data-height="20">90,80,-90,70,-61,83,63</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="pages/examples/invoice.html">OR1848</a></td>
+                  <td>Samsung Smart TV</td>
+                  <td><span class="label label-warning">Pending</span></td>
+                  <td>
+                    <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                  <td>iPhone 6 Plus</td>
+                  <td><span class="label label-danger">Delivered</span></td>
+                  <td>
+                    <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="pages/examples/invoice.html">OR9842</a></td>
+                  <td>Call of Duty IV</td>
+                  <td><span class="label label-success">Shipped</span></td>
+                  <td>
+                    <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.table-responsive -->
+          </div>
+          <!-- /.box-body -->
+        </div>  
+      </div>
+      <div class="col-md-4">
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">Latest Orders</h3>
+          </div>
+         <div class="box-body">
+          <img src="../../static/img/photo1.png" class="img-responsive pad">
+         </div>
+        </div>  
+      </div>
+    </row>
+  </section>
+</template>
+
+<script>
+import VACalendar from '../widgets/VACalendar.vue'
+import VAChart from '../components/VAChart'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msgHello: 'Hello',
+      msgBye: 'Bye',
+      loading: false,
+      barChartConfig: {
+        type: 'bar',
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+            {
+              label: 'My First dataset',
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1,
+              data: [65, 59, 80, 81, 56, 55, 40]
+            }
+          ]
+        },
+        options: {
+          scales: {
+            xAxes: [{
+              stacked: true
+            }],
+            yAxes: [{
+              stacked: true
+            }]
+          }
+        }
+      },
+      doughnutChartConfig: {
+        type: 'doughnut',
+        data: {
+          labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+          ],
+          datasets: [
+            {
+              data: [300, 50, 100],
+              backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+              ],
+              hoverBackgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+              ]
+            }]
+        },
+        options: {
+          animation: {
+            animateScale: true
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    clickBtn () {
+      this.loading = !this.loading
+      setTimeout(() => {
+        this.loading = !this.loading
+      }, 1000)
+    }
+  },
+  components: {
+    'va-calendar': VACalendar,
+    'va-chart': VAChart
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+/* img{
+  height: 25%;
+  width: 25%;
+} */
+</style>
